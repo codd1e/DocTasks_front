330fc66ad1a5539fdbf371d59ab8ade4e35900f5
codd1e <andreinik32@gmail.com>
2023-03-08 17:42:36 +0300

    added error auth alert

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,13 +1,20 @@
-import React, {FC, FormEvent, useState} from 'react';
+import React, {FC, FormEvent, useEffect, useState} from 'react';
 
 import {loginUser} from "../store/auth/actionCreators";
 import {useAppDispatch} from "../store";
 import TextField from "@mui/material/TextField";
+import {useSelector} from "react-redux";
+import {selectLoggedInError} from "../selectors/isLogged";
+import {Alert} from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from '@mui/icons-material/Close';
 
 const Login: FC = () => {
     const dispatch = useAppDispatch();
+    const [open, setOpen] = React.useState(true);
     const [login, setLogin] = useState<string>('');
     const [password, setPassword] = useState<string>('');
+    const isError = useSelector(selectLoggedInError)
     const handleSubmit = (event: FormEvent) => {
         event.preventDefault();
         dispatch(loginUser({login, password}));
@@ -38,6 +45,20 @@ const Login: FC = () => {
                 />
                 <button className="auth__form__btn" onClick={handleSubmit}>Вход</button>
             </form>
+            {isError ? <Alert variant="filled" severity="error" className="error_alert" action={
+                <IconButton
+                    aria-label="close"
+                    color="inherit"
+                    size="small"
+                    onClick={() => {
+                        setOpen(false);
+                    }}
+                >
+                    <CloseIcon fontSize="inherit" />
+                </IconButton>
+            }>
+                Что-то пошло не так! Попробуйте снова
+            </Alert> : null}
         </>
     );
 };
--- src/selectors/isLogged.ts
+++ src/selectors/isLogged.ts
@@ -2,4 +2,5 @@ import {RootState} from "../store";
 
 export const selectIsLoggedIn = ((state: RootState) => !!state.auth.authData.accessToken);
 
+export const selectLoggedInError = ((state: RootState) => !!state.auth.authData.error)
 export const selectCurrentRole = ((state: RootState) => state.auth.profileData.profile.role)
